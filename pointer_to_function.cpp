8dd66bb70af0fdb9a194379bd14ce4ff68f4015a
varunsaran01 <148057866+varunsaran01@users.noreply.github.com>
2023-10-17 14:58:50 +0530

    Create pointer_to_function.cpp

---
+++ pointer_to_function.cpp
@@ -0,0 +1,24 @@
+/*
+PRN - 22070123130
+POINTERS IN FUNCTION
+*/
+#include<iostream>
+using namespace std;
+
+void increment(int* a)
+{
+    ++(*a);
+}
+
+int main()
+{
+    int a = 2;
+    increment(&a);
+    cout << a << endl;
+    return 0;
+}
+
+/*
+Output:-
+3
+*/
